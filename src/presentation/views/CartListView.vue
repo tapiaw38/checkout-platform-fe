36ec2d1cb2a803d70a504230d25f404a9a61d0fb
tapiaw38 <tapiaw38@gmail.com>
2024-11-23 18:46:09 -0300

    first commit

---
+++ src/presentation/views/CartListView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Cart List</h1>
+    <p>This is the cart list page</p>
+  </main>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+              @import "@/presentation/assets/scss/_main.scss";
+              @import "@/presentation/assets/scss/_variables.scss";
+            `
+      }
+    }
+  }
+})
